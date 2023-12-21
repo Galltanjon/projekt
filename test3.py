99fe4b922690131c90ca56163e7df5ec0bfaf566
Galltanjon <139705241+Galltanjon@users.noreply.github.com>
2023-12-21 04:37:08 +0100

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,126 +1,117 @@
-
-class Ingredient:
-    def __init__(self, name, cost):
-        self.name = name
-        self.cost = cost
-
-class Dressing:
-    def __init__(self, name, cost):
-        self.name = name
-        self.cost = cost
-
-
-
-def user_freetext():
-    """Allows the user to freely write what they want in their salad (modifications included here)"""
-    user_ingredients = []
-    i = 0
-    print("What would you like to have in your salad?\nSeparate your input with enter")
-    print("When you're satisfied with your input:\n>Press 1 to find matching alternatives\nOtherwise:\n>Press 2 to remove latest ingredient\n>Press 3 to quit\n")
-    while i == 0:
-        user_choice = (input(">"))
-        if user_choice == "1":
-            i += 1
-            print("salad matching...")
-            print("salad suggesting...")
-
-        elif user_choice == "2": 
-            try:
-                print("Removed " + user_ingredients[-1])
-                user_ingredients.pop()
-            except IndexError:
-                print("There are no ingredients to remove")
-
-        elif user_choice == "3":
-            i += 1
-            #quit
-
-        else:
-            user_ingredients.append(user_choice)
-            print(user_ingredients)
-
-
-
-
-
-
-        
-user_freetext()
-
-class Menu:
-    def __init__(self, number, name, ingredients, dressing, price):
-        self.number = number
-        self.name = name
+class MenuItem():
+    """Imports the menu from the text file 'menu.txt' and compares the user's input with existing options"""
+    def __init__(self, name, ingredients, price):
         self.ingredients = ingredients
-        self.dressing = dressing
+        self.name = name
         self.price = price
 
-        # Example usage
-        menu_items = []
-        with open("menu.txt", "r", encoding="utf-8") as f:
-            for line in f:
-                # Parse the line and create Menu instances
-                # ...
-                cleaned_line = line.strip()
-                print(cleaned_line)
-                # Append Menu instance to the list
-                menu_items.append(Menu(number, name, ingredients, dressing, price))
-
-        print(menu_items)
-    __init__()
-
-class Order:
-    def __init__(self):
-        self.ingredients = []
-        self.dressing = None
-        self.modifications = []
-        self.price = 0
-
     def __str__(self):
-        """Returns a string representation of the salad"""
+        return f"{self.name}\nIngredients: {', '.join(self.ingredients)}\nPrice: {self.price} SEK"
 
-        return f"{self.ingredients} {self.modifications} {self.dressing} {self.price}"
 
-    def calculate_cost(self):
-        for ingredient in self.ingredients:
-            self.price += ingredient.cost
 
-        if self.dressing:
-            self.price += self.dressing.cost
+def read_menu(file_path):
+    """Reads menu text file 4 lines at a time and stores them as instances of Menu in menus list."""
 
-        # Add cost calculation for modifications
-        # ...
+    menus = []
+    with open(file_path, "r", encoding="utf-8") as f:
+        lines = f.read().splitlines()
+    for i in range(0, len(lines), 3):
+        name = lines[i]
+        ingredients = lines[i + 1].split(', ')
+        ingredients.sort()
+        price = int(lines[i + 2])
+        menu = MenuItem(name, ingredients, price)
+        menus.append(menu)
+    return menus
 
-def suggest_salad(user_ingredients, menu_items):
-    # Implement logic to find matches or suggestions
-    # ...
 
-    # Return the best match or suggestions
-    # ...
-    return suggest_salad
 
-def user_interaction():
-    order = Order()
 
-    while True:
-        user_choice = input("Enter your choice: ")
+class Options():
+    def __init__(self, ingredients, price):
+        self.ingredients = ingredients
+        self.price = price
 
-        if user_choice == "1":
-            print("Finding matches...")
-            suggest_salad(order.ingredients, menu_items)
-            order.calculate_cost()
-            print(f"Total cost: {order.price}")
+    def __str__(self): 
+        return f"{self.ingredients}\nPrice: {self.price} SEK"
+    
 
-        elif user_choice == "2":
-            order.remove_latest_ingredient()
-            order.calculate_cost()
+def read_options(file_path):
+    """Reads addons text file 2 lines at a time and stores them as instances of addons in addons list."""  
+    options = []
+    with open(file_path, "r", encoding="utf-8") as f: 
+        lines = f.read().splitlines()
+    for i in range(0, len(lines), 2):
+        ingredients = str(lines[i])
+        price = int(lines[i + 1])
+        option = Options(ingredients, price)
+        options.append(option)
+    return options
 
-        elif user_choice == "3":
+
+def user_wishes(bar, options):
+    wishes = []
+    ingredients = []
+    print(f"Welcome to salad bar {bar}\nEnter the ingredients you want\nWrite done when finished\n>")  
+    while ingredients != "done":
+        ingredients = input("Ingredients: ").lower()
+        if ingredients == "done":
             break
-
+        elif ingredients not in [option.ingredients for option in options]: #chatGPT used for part after 'not in'
+            print("This ingredient is not available")
         else:
-            order.add_ingredient(user_choice)
-            print(order)
+            wishes.append(ingredients)       
+    print(wishes) #to be removed
+    return wishes
 
-user_interaction()
 
+
+def find_best_match(user_wishes, menus):
+    best_match = None
+    max_matching_ingredients = 0
+
+    for menu in menus:
+        matching_ingredients = len(set(user_wishes).intersection(menu.ingredients)) #chatGPT used for matching function
+        
+        if matching_ingredients > max_matching_ingredients:
+            max_matching_ingredients = matching_ingredients
+            best_match = menu
+        elif matching_ingredients == max_matching_ingredients and menu.price < best_match.price:
+            best_match = menu
+
+    if best_match:
+        needed_ingredients = set(best_match.ingredients) - set(user_wishes)
+        return best_match, needed_ingredients
+    else:
+        return None, None
+
+
+
+def main():
+    bar = input("Write 1 for the first salad bar or 2 for the second\n>")
+    menus = read_menu(f"{bar}menu.txt")
+    options = read_options(f"{bar}options.txt")
+
+    for menu in menus:
+        print(menu)
+        print("------------------------")
+
+    for option in options:
+        print(option)
+        print("------------------------")
+
+    user_wishes_list = user_wishes(bar, options)
+
+    best_match, needed_ingredients = find_best_match(user_wishes_list, menus)
+
+    if best_match:
+        print(f"\nBest Match: {best_match.name}\nPrice: {best_match.price} SEK")
+        print("Needed Ingredients:")
+        for ingredient in needed_ingredients:
+            print(ingredient)
+    else:
+        print("No matching item found.")
+
+if __name__ == "__main__":
+    main()
