5fd5d03e9a7697f53b187d3bba27772c6f670caa
Galltanjon <139705241+Galltanjon@users.noreply.github.com>
2023-12-27 12:53:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -164,76 +164,78 @@ def receipt(best_match, addons_wishes, extra_ingredients, total_order_cost):
     with open("receipt.txt", "w") as receipt_file:
         receipt_file.write(f"{best_match.name}: {best_match.price} SEK\n")
 
+        #Chosen at checkout
         for addon, addon_price in addons_wishes:
-            receipt_file.write(f"{addon}: {addon_price} SEK\n")
+            receipt_file.write(f"+{addon}: {addon_price} SEK\n")
 
+        #Chosen during order
         if extra_ingredients:
             receipt_file.write("\nExtra Ingredients:\n")
             for ingredient, extra_ingredient_cost in extra_ingredients.items():
                 receipt_file.write(f"{ingredient}: {extra_ingredient_cost} SEK\n")
 
-            # Calculate the total cost of extra ingredients
+            #Calculate the total cost of extra ingredients
             extra_ingredients_cost = sum(extra_ingredients.values())
             receipt_file.write(f"Additional cost for extra ingredients: {extra_ingredients_cost} SEK\n")
 
         receipt_file.write(f"Total cost: {total_order_cost} SEK")
 
+def display_best_match(best_match, needed_ingredients, extra_ingredients):
+    """Displays information about the best match, needed ingredients, and extra ingredients.
+    If there is a best match, extra_ingredient_cost is returned, otherwise 0 (None) is returned"""
+    if best_match:
+        print(f"\nBest Match: {best_match.name}\nPrice: {best_match.price} SEK")
+        if needed_ingredients:
+            print("Needed Ingredients:")
+            for ingredient in needed_ingredients:
+                print(ingredient)
+        if extra_ingredients:
+            print("\nExtra Ingredients:")
+            for ingredient in extra_ingredients:
+                print(ingredient)
+            #Calculate the cost of extra ingredients
+            extra_ingredient_cost = find_extra_ingredient_cost(extra_ingredients, options)
+            print(f"Additional cost for extra ingredients: {extra_ingredient_cost} SEK")
+            return extra_ingredient_cost
+    return 0
+
 def main(menus, options):
-    """Takes user input to select a salad bar.
-    Reads menu and options from the selected bar.
+    """Initiates menu read and options from the selected bar.
     Takes user input for the main order and addons.
     Displays the best match for the main order and needed ingredients.
     Provides options to complete the order, add addons, or start a new order.
     Prints a receipt based on the completed order."""
 
     total_order_cost = 0
+    addons_wishes = []
+    extra_ingredients = set()
+
+    user_wishes_list = user_wishes(options)
+
+    best_match, needed_ingredients, extra_ingredients = find_best_match(user_wishes_list, menus, options)
+
+    #Displays information about the best match, needed ingredients, and extra ingredients
+    extra_ingredient_cost = display_best_match(best_match, needed_ingredients, extra_ingredients)
+    total_order_cost += extra_ingredient_cost
 
     while True:
-        order_cost = 0
-        addons_wishes = []  
-        extra_ingredients = set()
-
-        user_wishes_list = user_wishes(options)
-        best_match, needed_ingredients, extra_ingredients = find_best_match(user_wishes_list, menus, options)
-
-        if best_match:
-            print(f"\nBest Match: {best_match.name}\nPrice: {best_match.price} SEK")
-            if needed_ingredients:
-                print("Needed Ingredients:")
-                for ingredient in needed_ingredients:
-                    print(ingredient)
-            if extra_ingredients:
-                print("\nExtra Ingredients:")
-                for ingredient in extra_ingredients:
-                    print(ingredient)
-                # Calculate the cost of extra ingredients
-                extra_ingredient_cost = find_extra_ingredient_cost(extra_ingredients, options)
-                print(f"Additional cost for extra ingredients: {extra_ingredient_cost} SEK")
-                order_cost += extra_ingredient_cost
-
-            while True:
-                accept_order = input("'yes' to complete the main order, 'no' to add addons, 'q' to quit\n>")
-                if accept_order == "yes":
-                    order_cost += best_match.price
-                    total_order_cost += order_cost
-                    break  # Exit the loop after successfully processing the order
-                elif accept_order == "no":
-                    order_cost, addons_wishes = addons(options, user_wishes_list, order_cost)
-                    order_cost += best_match.price
-                    total_order_cost += order_cost
-                    break  # Exit the loop after successfully processing the order
-                elif accept_order == "q":
-                    sys.exit()
-                else:
-                    print("Write 'yes', 'no' or 'new'")
+        accept_order = input("'yes' to complete the main order, 'no' to add addons, 'q' to quit\n>")
+        if accept_order == "yes":
+            total_order_cost += best_match.price
+            break  
+        elif accept_order == "no":
+            total_order_cost, addons_wishes = addons(options, user_wishes_list, total_order_cost)
+            total_order_cost += best_match.price
+            break  
+        elif accept_order == "q":
+            sys.exit()
         else:
-            print("No suggestion found. Try a new order")
+            print("Write 'yes', 'no' or 'new'")
 
-        receipt(best_match, addons_wishes, extra_ingredients, order_cost)
+    receipt(best_match, addons_wishes, extra_ingredients, total_order_cost)
 
-        print("Here is the receipt")
-        print("Enjoy!")
-        break
+    print("Here is the receipt")
+    print("Enjoy!")
 
 if __name__ == "__main__":
     menus, options = choose_bar()
